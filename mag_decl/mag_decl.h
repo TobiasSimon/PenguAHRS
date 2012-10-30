66c517aba1a9aaf87a297853f43abc6aee707689
Tobias Simon <tobias.simon@tu-ilmenau.de>
2012-10-30 16:27:54 +0100

    added code for magnetic declination from ardupilot

---
+++ mag_decl/mag_decl.h
@@ -0,0 +1,16 @@
+
+#ifndef __MAG_DECL_H__
+#define __MAG_DECL_H__
+
+/*
+ *	Adam M Rivera
+ *	With direction from: Andrew Tridgell, Jason Short, Justin Beech
+ *
+ *	Adapted from: http://www.societyofrobots.com/robotforum/index.php?topic=11855.0
+ *	Scott Ferguson
+ *	scottfromscott@gmail.com
+ *
+ */
+float get_declination(float lat, float lon);
+
+#endif // __MAG_DECL_H__
